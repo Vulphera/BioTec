16091d5d44a75483621cc22e682f00193d86dd81
Marcel Martelo <marcelmartelo@gmail.com>
2025-03-07 17:08:44 -0300

    1° exercício
    primeiro exercício proposto feito

---
+++ Biotec_otimizado.py
@@ -0,0 +1,24 @@
+#%%
+#Escreva um programa que recebe uma sequência de DNA e retorna o fio complementar (A ↔ T, C ↔ G).
+
+
+def complemento_fita(fita):
+    fita = fita.upper()
+    complemento = []
+    bases_validas = {'A', 'T', 'C', 'G', 'U'}
+    bases_invalidas = set(fita) - bases_validas
+
+    if bases_invalidas:
+        return f'Bases inválidas encontradas: {', '.join(bases_invalidas)}. Verifique a sequência'
+    if 'U' in fita:
+        return 'A fita apresentada é parte de um RNA'
+    
+    pares = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    complemento = ''.join(pares[n] for n in fita)
+    return complemento
+
+
+print(complemento_fita("atcg"))
+print(complemento_fita("AUCG"))
+print(complemento_fita("ATXG"))
+print(complemento_fita("ATCGTAC"))
---
+++ Complemento_de_DNA.py
@@ -0,0 +1,92 @@
+#%%
+#Escreva um programa que recebe uma sequência de DNA e retorna o fio complementar (A ↔ T, C ↔ G).
+
+
+def complementoDNA(fita):
+    fita = fita.upper()
+    complemento = []
+    base = 0
+    for n in fita:
+        base += 1
+        if n == 'A':
+            complemento.append('T')
+        elif n == 'T':
+            complemento.append('A')
+        elif n == 'C':
+            complemento.append('G')
+        elif n == 'G':
+            complemento.append('C')
+        elif n == 'U':
+            return 'A fita apresentada é parte de um RNA'
+        else:
+            return f'A base nitrogenada {n} foi digitada de forma errada, por favor verifique: a base {base} '
+            #futuramente inserir qual das bases foi digitada de forma errada
+    return ''.join(complemento)
+
+print(complementoDNA('ATCG'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+#Crie um programa que recebe uma sequência de DNA e retorna a contagem de cada base nitrogenada.
+
+
+#%%
+#Transforme uma sequência de DNA em RNA substituindo T por U.
+
+#%%
+#A proporção GC indica a quantidade de Guanina (G) e Citosina (C) em relação ao tamanho total da sequência.
+
